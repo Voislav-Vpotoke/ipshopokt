547709cdec57d7b5cc54f804e3de5ccd04372110
Voislav-Vpotoke <vpotoke.om@gmail.com>
2022-10-14 23:37:33 +0400

    webreact

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+const tg = window.Telegram.WebApp;
+function App() {
+  const onClose = () => {
+    f
+  }
+  return (
+    <div className="App">
+
+      work
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+export default App;
+
