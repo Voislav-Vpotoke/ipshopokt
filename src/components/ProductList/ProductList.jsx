34bd6c5becc57792ee130cb5a2f16072808ac0bf
Voislav-Vpotoke <vpotoke.om@gmail.com>
2022-10-15 22:50:38 +0400

    webreact

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000/web-data', {
+        fetch('http://localhost:8000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
