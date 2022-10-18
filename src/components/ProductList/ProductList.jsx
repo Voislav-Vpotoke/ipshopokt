df241e210620b4a6232065b1ec5cf2e0bae85352
Voislav-Vpotoke <vpotoke.om@gmail.com>
2022-10-18 14:20:56 +0400

    webreact

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://62.113.111.19:8000/web-data', {
+        fetch('http://62.113.111.19:8002/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
