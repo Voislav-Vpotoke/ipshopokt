ebab900893e71a6579e1f6e4a319678b3008d506
Voislav-Vpotoke <vpotoke.om@gmail.com>
2022-10-18 13:21:44 +0400

    webreact

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000', {
+        fetch('http://62.113.111.19:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
